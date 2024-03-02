0d337a9aa37a7cbc0e4933d883948a3184aefe75
kain <zhangtqx@vip.qq.com>
2024-03-02 20:39:43 +0800

    update: init

---
+++ src/test/java/com/weplanx/insight/InsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weplanx.insight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
