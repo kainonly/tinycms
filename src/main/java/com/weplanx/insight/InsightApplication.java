1f3471e4c2776b7d7f2b5b36a36159d01d97bff2
kain <zhangtqx@vip.qq.com>
2024-03-02 22:17:49 +0800

    update: init

--- src/main/java/com/weplanx/insight/InsightApplication.java
+++ src/main/java/com/weplanx/insight/InsightApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InsightApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(InsightApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InsightApplication.class, args);
+    }
 
 }
