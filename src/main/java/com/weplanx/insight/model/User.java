c845f760ff7f4cce34cef9cb7d497ec63555ed44
kain <zhangtqx@vip.qq.com>
2024-03-07 22:04:02 +0800

    update: test

--- src/main/java/com/weplanx/insight/model/User.java
+++ src/main/java/com/weplanx/insight/model/User.java
@@ -2,14 +2,19 @@ package com.weplanx.insight.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
+import java.util.Date;
 
 @Entity
 public class User {
     @Id
     @GeneratedValue
     private Long id;
-    private String username;
+    private String email;
     private String password;
+    private String name;
+    private Boolean status;
+    private Date createdAt;
+    private Date updatedAt;
 }
